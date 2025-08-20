b122491a83075b2db921ee7287948b472a39621f
rafafuentesz <rafafuentes110@gmail.com>
2025-08-20 10:22:31 -0300

    Agregar archivos de entorno para la configuración de la aplicación Gifs

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = {
+
+  production: true,
+  companyName: 'Gifs',
+  companyName2: 'App',
+  companySlogan: 'Maneja tus gifs',
+
+  //apikeys
+giphyApiKey: 'Zi5Iz63lAxEhWN7TSHPtGHvuECunwNJj',
+  giphyUrl: 'https://api.giphy.com/v1',
+
+  //urls
+};
